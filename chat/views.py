62001dd87d67960cffe726d29d62e37a5afb3fd4
masahiro.yamada <xwrokmailmasa@gmail.com>
2023-04-20 00:09:09 +0900

    API連携の土台(未完成)

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+import openai, os
+from dotenv import load_dotenv
+load_dotenv()
 
+api_key = os.getenv("OPENAI_KEY", None)
 # Create your views here.
 def frontpage(request):
-    return render(request, "chat/frontpage.html")
+    chatbot_responce = None
+    if api_key is not None and request.method == 'POST':
+        open.api_key = api_key
+        user_input = request.POST.get('user_input')
+        prompt = user_input
+
+        response = openai.Completion.create(
+            engine = 'text-davinci-003',
+            prompt=prompt,
+            max_token=256,
+            # stop='.'
+        )
+        print(response)
+    return render(request, "chat/frontpage.html",  {})

ed2f8dfcc22d4b839a8939d70d027450d14212be
masahiro.yamada <xwrokmailmasa@gmail.com>
2023-04-20 08:53:55 +0900

    OPENAI疎通土台(完成)環境変数にkeyを入れれば疎通できる

--- chat/views.py
+++ chat/views.py
@@ -5,18 +5,20 @@ load_dotenv()
 
 api_key = os.getenv("OPENAI_KEY", None)
 # Create your views here.
-def frontpage(request):
-    chatbot_responce = None
+def chatbot(request):
+    chatbot_response = None
     if api_key is not None and request.method == 'POST':
-        open.api_key = api_key
+        openai.api_key = api_key
         user_input = request.POST.get('user_input')
         prompt = user_input
 
         response = openai.Completion.create(
             engine = 'text-davinci-003',
             prompt=prompt,
-            max_token=256,
+            max_tokens=256,
             # stop='.'
         )
-        print(response)
-    return render(request, "chat/frontpage.html",  {})
+        print("API response:", response)  # Add this line
+        chatbot_response = response["choices"][0]["text"].strip()
+        print("Chatbot response:", chatbot_response)  # Add this line
+    return render(request, "chat/frontpage.html",  {"response": chatbot_response})
--- chatsite/urls.py
+++ chatsite/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-
-from chat.views import frontpage
+from chat.views import chatbot
+# from chat.views import frontpage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", frontpage)
+    path("",chatbot, name="chatbot" ),
 ]
